52fb5fc7f14133fec021477930f59dab4ed594c3
Dnny <mcmillandanny@gmail.com>
2019-05-30 15:13:43 -0400

    adjusted / added content

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,10 @@
 function clearBox() {
     var card1Result = document.querySelector(".card1-result").innerHTML = "";
     var card2Result = document.querySelector(".card2-result").innerHTML = "";
+    var card3Result = document.querySelector(".card3-result").innerHTML = "";
+    var card4Result = document.querySelector(".card4-result").innerHTML = "";
+    var card5Result = document.querySelector(".card5-result").innerHTML = "";
+    var card6Result = document.querySelector(".card6-result").innerHTML = "";
 }
 
 var card1 = document.querySelector(".card-1");
@@ -11,6 +15,7 @@ var card3 = document.querySelector(".card-3");
 var card4 = document.querySelector(".card-4");
 var card5 = document.querySelector(".card-5");
 var card6 = document.querySelector(".card-6");
+var card7 = document.querySelector(".card-7");
 var bac = void 0; // BLOOD ALCOHOL TOTAL
 var H = void 0; // HOURS
 var R; // ALCOHOL DISTRIBUTION RATIO
@@ -57,6 +62,7 @@ var totalConsumed = function totalConsumed() {
 };
 
 // get hours elapsed
+
 var hoursInputs = document.querySelector('.hours-wrap');
 
 var hoursTotal = function hoursTotal() {
@@ -91,6 +97,8 @@ var hoursTotal = function hoursTotal() {
                 card4.style.display = "none";
                 card5.style.display = "none";
                 card6.style.display = "none";
+                card6.style.display = "none";
+                card7.style.display = "block";
         }
 
         switch (bac) {
@@ -108,6 +116,7 @@ var hoursTotal = function hoursTotal() {
                 card4.style.display = "none";
                 card5.style.display = "none";
                 card6.style.display = "none";
+                card7.style.display = "none";
                 var card1Result = document.querySelector(".card1-result");
                 card1Result.appendChild(bacResult);
         }
@@ -127,6 +136,7 @@ var hoursTotal = function hoursTotal() {
                 card4.style.display = "none";
                 card5.style.display = "none";
                 card6.style.display = "none";
+                card7.style.display = "none";
                 var card2Result = document.querySelector(".card2-result");
                 card2Result.appendChild(bacResult);
 
@@ -148,187 +158,59 @@ var hoursTotal = function hoursTotal() {
                 card4.style.display = "none";
                 card5.style.display = "none";
                 card6.style.display = "none";
+                card7.style.display = "none";
                 var card3Result = document.querySelector(".card3-result");
                 card3Result.appendChild(bacResult);
+        }
+
+        if (bac >= 0.11 && bac <= 0.20) {
+            TweenMax.fromTo(".card-4", .3, {
+                y: "70px",
+                display: "inline-block"
+            }, {
+                y: "0px"
+            });
+            card1.style.display = "none";
+            card2.style.display = "none";
+            card3.style.display = "none";
+            card5.style.display = "none";
+            card6.style.display = "none";
+            card7.style.display = "none";
+            var card4Result = document.querySelector(".card4-result");
+            card4Result.appendChild(bacResult);
+        } else if (bac >= 0.21 && bac <= 0.34) {
+            TweenMax.fromTo(".card-5", .3, {
+                y: "70px",
+                display: "inline-block"
+            }, {
+                y: "0px"
+            });
+            card1.style.display = "none";
+            card2.style.display = "none";
+            card3.style.display = "none";
+            card4.style.display = "none";
+            card6.style.display = "none";
+            card7.style.display = "none";
+            var card5Result = document.querySelector(".card5-result");
+            card5Result.appendChild(bacResult);
+        } else if (bac >= 0.35) {
+            TweenMax.fromTo(".card-6", .3, {
+                y: "70px",
+                display: "inline-block"
+            }, {
+                y: "0px"
+            });
+            card1.style.display = "none";
+            card2.style.display = "none";
+            card3.style.display = "none";
+            card4.style.display = "none";
+            card5.style.display = "none";
+            card7.style.display = "none";
+            var card6Result = document.querySelector(".card6-result");
+            card6Result.appendChild(bacResult);
         }
     });
 };
 
 hoursTotal();
-
-// let card1Result = document.querySelector(".card1-result");
-
-
-// let Btn = document.querySelector('.submit-btn');
-
-
-// Btn.addEventListener('click', function getBAC() {
-
-//     clearBox();
-
-//     let bac;
-//     var gender =  document.querySelector('input[name="gender"]:checked');
-//     var R; // alcohol distribution ratio
-//     var hoursElapsed =  document.querySelectorAll('input[name="time"]:checked');
-//     var H; // time passed 
-//     var drinksAmountBeer =  document.querySelector('select[name=beer]').value;
-//     var drinksAmountWine =  document.querySelector('select[name=wine]').value;
-//     var drinksAmountSpirits =  document.querySelector('select[name=spirits]').value;
-//     var A; //amount consumed
-//     var weight = document.getElementById("weight-input").value;
-//     var W; // body weight
-//     W = parseInt(weight);
-
-//     let beer = drinksAmountBeer * 12 * 0.05;
-//     let wine = drinksAmountWine *  5 * 0.12;    
-//     let spirits = drinksAmountSpirits * 1.5 * 0.40;    
-
-//     let totalAlcohol = beer + wine + spirits;
-
-//     hoursElapsed.forEach(function(hour) {
-//     H = hour.value;
-//     console.log("hours elapsed" , + H);
-//     });
-
-//     if( gender.value === 'male') {
-//         console.log("MALE")
-//         var R = 0.73;
-//     }
-//     else  {
-//         console.log('FEMALE')
-//         var R = 0.66;
-//     }
-
-
-//     bac = ((totalAlcohol * 5.15 ) / (W * R)) - (.015 * H);
-//     bac = Math.round(bac * 100) / 100;
-//     console.log(bac);
-
-//     let bacResult = document.createElement('div');
-//     bacResult.classList.add("result");
-//     bacResult.innerText = bac;
-
-//     switch (bac) {
-//         case 0.00:
-//         case -0.01:
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//     }
-
-//     switch (bac) {
-//         case 0.01:
-//         case 0.02:
-//         case 0.03:
-//         TweenMax.fromTo(".card-1", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//         let card1Result = document.querySelector(".card1-result"); 
-//         card1Result.appendChild(bacResult);
-//     }
-
-//     switch (bac) {
-//         case 0.04:
-//         case 0.05:
-//         case 0.06:
-//         TweenMax.fromTo(".card-2", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//         let card2Result = document.querySelector(".card2-result"); 
-//         card2Result.appendChild(bacResult);
-
-//     }
-
-//     switch (bac) {
-//         case 0.07:
-//         case 0.08:
-//         case 0.09:
-//         case 0.10:
-//         TweenMax.fromTo(".card-3", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none"; 
-//     }
-
-//     if (bac >= 0.11 && bac <= 0.20) {
-//         TweenMax.fromTo(".card-4", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//     }
-
-//     else if (bac >= 0.21 && bac <= 0.34) {
-//         TweenMax.fromTo(".card-5", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card6.style.display = "none";
-//     }
-//     else if (bac >= 0.35) {
-//         TweenMax.fromTo(".card-6", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//     }  
-
-
-// })
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,11 @@
 function clearBox() {
     let card1Result = document.querySelector(".card1-result").innerHTML = "";
     let card2Result = document.querySelector(".card2-result").innerHTML = "";
+    let card3Result = document.querySelector(".card3-result").innerHTML = "";
+    let card4Result = document.querySelector(".card4-result").innerHTML = "";
+    let card5Result = document.querySelector(".card5-result").innerHTML = "";
+    let card6Result = document.querySelector(".card6-result").innerHTML = "";
+
 }
     
 
@@ -12,6 +17,7 @@ let card3 = document.querySelector(".card-3");
 let card4 = document.querySelector(".card-4");
 let card5 = document.querySelector(".card-5");
 let card6 = document.querySelector(".card-6");
+let card7 = document.querySelector(".card-7");
 let bac; // BLOOD ALCOHOL TOTAL
 let H;  // HOURS
 var R; // ALCOHOL DISTRIBUTION RATIO
@@ -62,6 +68,7 @@ let totalConsumed = function() {
 }
 
 // get hours elapsed
+
 var hoursInputs = document.querySelector('.hours-wrap');
 
 let hoursTotal = function() {
@@ -99,6 +106,8 @@ let hoursTotal = function() {
             card4.style.display = "none";
             card5.style.display = "none";
             card6.style.display = "none";
+            card6.style.display = "none";
+            card7.style.display = "block";
         }
     
         switch (bac) {
@@ -118,6 +127,7 @@ let hoursTotal = function() {
             card4.style.display = "none";
             card5.style.display = "none";
             card6.style.display = "none";
+            card7.style.display = "none";
             let card1Result = document.querySelector(".card1-result"); 
             card1Result.appendChild(bacResult);
         }
@@ -139,6 +149,7 @@ let hoursTotal = function() {
             card4.style.display = "none";
             card5.style.display = "none";
             card6.style.display = "none";
+            card7.style.display = "none";
             let card2Result = document.querySelector(".card2-result"); 
             card2Result.appendChild(bacResult);
     
@@ -163,8 +174,65 @@ let hoursTotal = function() {
             card4.style.display = "none";
             card5.style.display = "none";
             card6.style.display = "none";
+            card7.style.display = "none";
             let card3Result = document.querySelector(".card3-result"); 
             card3Result.appendChild(bacResult);
+        }
+
+        if (bac >= 0.11 && bac <= 0.20) {
+            TweenMax.fromTo(".card-4", .3, {
+                y: "70px",
+                display: "inline-block",
+            },
+                {
+                    y: "0px"
+                }
+            )
+            card1.style.display = "none";
+            card2.style.display = "none";
+            card3.style.display = "none";
+            card5.style.display = "none";
+            card6.style.display = "none";
+            card7.style.display = "none";
+            let card4Result = document.querySelector(".card4-result"); 
+            card4Result.appendChild(bacResult);
+        }
+
+        else if (bac >= 0.21 && bac <= 0.34) {
+            TweenMax.fromTo(".card-5", .3, {
+                y: "70px",
+                display: "inline-block",
+            },
+                {
+                    y: "0px"
+                }
+            )
+            card1.style.display = "none";
+            card2.style.display = "none";
+            card3.style.display = "none";
+            card4.style.display = "none";
+            card6.style.display = "none";
+            card7.style.display = "none";
+            let card5Result = document.querySelector(".card5-result"); 
+            card5Result.appendChild(bacResult);
+        }
+        else if (bac >= 0.35) {
+            TweenMax.fromTo(".card-6", .3, {
+                y: "70px",
+                display: "inline-block",
+            },
+                {
+                    y: "0px"
+                }
+            )
+            card1.style.display = "none";
+            card2.style.display = "none";
+            card3.style.display = "none";
+            card4.style.display = "none";
+            card5.style.display = "none";
+            card7.style.display = "none";
+            let card6Result = document.querySelector(".card6-result"); 
+            card6Result.appendChild(bacResult);
         }
     })
 
@@ -172,192 +240,3 @@ let hoursTotal = function() {
 
 hoursTotal();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let card1Result = document.querySelector(".card1-result");
-
-
-// let Btn = document.querySelector('.submit-btn');
-
-
-// Btn.addEventListener('click', function getBAC() {
-
-//     clearBox();
-
-//     let bac;
-//     var gender =  document.querySelector('input[name="gender"]:checked');
-//     var R; // alcohol distribution ratio
-//     var hoursElapsed =  document.querySelectorAll('input[name="time"]:checked');
-//     var H; // time passed 
-//     var drinksAmountBeer =  document.querySelector('select[name=beer]').value;
-//     var drinksAmountWine =  document.querySelector('select[name=wine]').value;
-//     var drinksAmountSpirits =  document.querySelector('select[name=spirits]').value;
-//     var A; //amount consumed
-//     var weight = document.getElementById("weight-input").value;
-//     var W; // body weight
-//     W = parseInt(weight);
-    
-//     let beer = drinksAmountBeer * 12 * 0.05;
-//     let wine = drinksAmountWine *  5 * 0.12;    
-//     let spirits = drinksAmountSpirits * 1.5 * 0.40;    
-
-//     let totalAlcohol = beer + wine + spirits;
-      
-//     hoursElapsed.forEach(function(hour) {
-//     H = hour.value;
-//     console.log("hours elapsed" , + H);
-//     });
-  
-//     if( gender.value === 'male') {
-//         console.log("MALE")
-//         var R = 0.73;
-//     }
-//     else  {
-//         console.log('FEMALE')
-//         var R = 0.66;
-//     }
-
-
-//     bac = ((totalAlcohol * 5.15 ) / (W * R)) - (.015 * H);
-//     bac = Math.round(bac * 100) / 100;
-//     console.log(bac);
-
-//     let bacResult = document.createElement('div');
-//     bacResult.classList.add("result");
-//     bacResult.innerText = bac;
-
-//     switch (bac) {
-//         case 0.00:
-//         case -0.01:
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//     }
-
-//     switch (bac) {
-//         case 0.01:
-//         case 0.02:
-//         case 0.03:
-//         TweenMax.fromTo(".card-1", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//         let card1Result = document.querySelector(".card1-result"); 
-//         card1Result.appendChild(bacResult);
-//     }
-
-//     switch (bac) {
-//         case 0.04:
-//         case 0.05:
-//         case 0.06:
-//         TweenMax.fromTo(".card-2", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//         let card2Result = document.querySelector(".card2-result"); 
-//         card2Result.appendChild(bacResult);
-            
-//     }
-
-//     switch (bac) {
-//         case 0.07:
-//         case 0.08:
-//         case 0.09:
-//         case 0.10:
-//         TweenMax.fromTo(".card-3", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none"; 
-//     }
-
-//     if (bac >= 0.11 && bac <= 0.20) {
-//         TweenMax.fromTo(".card-4", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card5.style.display = "none";
-//         card6.style.display = "none";
-//     }
-
-//     else if (bac >= 0.21 && bac <= 0.34) {
-//         TweenMax.fromTo(".card-5", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card6.style.display = "none";
-//     }
-//     else if (bac >= 0.35) {
-//         TweenMax.fromTo(".card-6", .3, {
-//             y: "70px",
-//             display: "inline-block",
-//         },
-//             {
-//                 y: "0px"
-//             }
-//         )
-//         card1.style.display = "none";
-//         card2.style.display = "none";
-//         card3.style.display = "none";
-//         card4.style.display = "none";
-//         card5.style.display = "none";
-//     }  
-
-
-// })  
